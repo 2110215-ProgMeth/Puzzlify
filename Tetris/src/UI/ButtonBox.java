56a5d8b06f4e4c0716557bd85b8a1a6dc39c77bc
paopeaw <paopeaw8246@gmail.com>
2023-12-10 01:32:48 +0700

    update ui

--- Tetris/src/UI/ButtonBox.java
+++ Tetris/src/UI/ButtonBox.java
@@ -1,9 +1,12 @@
 package UI;
 
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,9 +18,18 @@ public class ButtonBox implements Initializable {
     private String disableButtonPath;
     private Image enableImg = new Image("/UISprite/StartButtonEnable.png");
     private Image disableImg = new Image("/UISprite/StartButtonDisable.png");
+    private TranslateTransition translateTransition = new TranslateTransition();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         imageView.setImage(enableImg);
+        translateTransition.setNode(imageView);
+        translateTransition.setDuration(Duration.millis(1000));
+        translateTransition.setByY(10);
+
+        translateTransition.setInterpolator(Interpolator.EASE_BOTH);
+        translateTransition.setCycleCount(-1);
+        translateTransition.setAutoReverse(true);
+        translateTransition.play();
     }
 
     public void mousePressed(){
--- Tetris/src/UI/ScoreBox.java
+++ Tetris/src/UI/ScoreBox.java
@@ -2,11 +2,13 @@ package UI;
 
 import javafx.animation.Interpolator;
 import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
+import javax.xml.crypto.dsig.TransformService;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,9 +18,29 @@ public class ScoreBox implements Initializable {
     @FXML
     private Text labelText;
     ScaleTransition scaleTransition = new ScaleTransition();
-
+    TranslateTransition translateTransition = new TranslateTransition();
+    TranslateTransition translateTransitionS = new TranslateTransition();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        translateTransition.setNode(labelText);
+        translateTransition.setDuration(Duration.millis(1000));
+        translateTransition.setByY(10);
+
+        translateTransition.setInterpolator(Interpolator.EASE_BOTH);
+        translateTransition.setCycleCount(-1);
+        translateTransition.setAutoReverse(true);
+        translateTransition.play();
+
+        translateTransitionS.setNode(scoreText);
+        translateTransitionS.setDuration(Duration.millis(1000));
+        translateTransitionS.setByY(10);
+
+        translateTransitionS.setInterpolator(Interpolator.EASE_BOTH);
+        translateTransitionS.setCycleCount(-1);
+        translateTransitionS.setAutoReverse(true);
+        translateTransitionS.play();
+
+
         scaleTransition.setNode(scoreText);
         scaleTransition.setDuration(Duration.millis(100));
         scaleTransition.setFromX(.8);
